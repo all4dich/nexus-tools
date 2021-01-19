23215a1554a63ad291273034b227c18607760b24
Sunjoo Park <all4dich@gmail.com>
2021-01-19 11:20:15 +0900

    Rename directory and package title for this module

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+python_src_root = "src/main/python"
 setup(
     name='nexus-tools',
+    version='0.0.1',
     zip_safe=False,
     description='Tools in order to  manage Nexus OSS repository',
     author='Sunjoo Park',
     author_email='all4dich@gmail.com',
-    packages=['swtools'],
-    package_dir={'': 'src'}
+    package_dir={'': python_src_root},
+    packages=find_packages(where=python_src_root)
 )
